6b5a2f2bdf8ce4d1fb7e57ab397a4b393ad60253
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-08-09 17:37:55 +1000

    Arrows working.

--- image_slider.js
+++ image_slider.js
@@ -2,7 +2,8 @@ var currentImage = 1;
 
 $(document).ready(function() {
 
-	$('#pictureFrame').on('click', next)
+	$('#rightArrow').on('click', next)
+	$('#leftArrow').on('click', previous)
 
 });
 
@@ -18,4 +19,14 @@ function next() {
 	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black
 };
 
-
+function previous() {
+	$("#dot" + currentImage).css("background-color", "white"); //change current dot colour back to white
+	if (currentImage >= 2) {
+		currentImage = currentImage - 1
+	} else {
+		currentImage = 3
+		$("#imageDiv").css({marginLeft:'-1800px'})
+	}
+	$("#imageDiv").animate({marginLeft:'+=600px'}, "fast"); //slide image left
+	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black
+};
