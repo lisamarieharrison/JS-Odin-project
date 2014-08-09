5ae09644b7a068c1db9edd2bb521ad9d8ae92b13
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-08-09 17:51:23 +1000

    Dots working.

--- image_slider.js
+++ image_slider.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 
 	$('#rightArrow').on('click', next)
 	$('#leftArrow').on('click', previous)
+	$('#dot1').on('click', move1)
+	$('#dot2').on('click', move2)
+	$('#dot3').on('click', move3)
 
 });
 
@@ -30,3 +33,39 @@ function previous() {
 	$("#imageDiv").animate({marginLeft:'+=600px'}, "fast"); //slide image left
 	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black
 };
+
+function move1() {
+	$("#dot" + currentImage).css("background-color", "white"); //change current dot colour back to white
+	if (currentImage == 2) {
+		$("#imageDiv").animate({marginLeft:'+=600px'}, "fast"); //slide image left
+	}
+	if (currentImage == 3) {
+		$("#imageDiv").animate({marginLeft:'+=1200px'}, "fast"); //slide image left
+	}
+	currentImage = 1
+	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black	
+}
+
+function move2() {
+	$("#dot" + currentImage).css("background-color", "white"); //change current dot colour back to white
+	if (currentImage == 1) {
+		$("#imageDiv").animate({marginLeft:'-=600px'}, "fast"); //slide image left
+	}
+	if (currentImage == 3) {
+		$("#imageDiv").animate({marginLeft:'+=600px'}, "fast"); //slide image left
+	}
+	currentImage = 2
+	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black	
+}
+
+function move3() {
+	$("#dot" + currentImage).css("background-color", "white"); //change current dot colour back to white
+	if (currentImage == 1) {
+		$("#imageDiv").animate({marginLeft:'-=1200px'}, "fast"); //slide image left
+	}
+	if (currentImage == 2) {
+		$("#imageDiv").animate({marginLeft:'-=600px'}, "fast"); //slide image left
+	}
+	currentImage = 3
+	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black	
+}
