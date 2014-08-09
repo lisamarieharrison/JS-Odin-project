a7f115b6522ac284dcccee240650ad6b252b5073
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-08-09 17:22:11 +1000

    Dots now working.

--- image_slider.js
+++ image_slider.js
@@ -7,14 +7,15 @@ $(document).ready(function() {
 });
 
 function next() {
+	$("#dot" + currentImage).css("background-color", "white"); //change current dot colour back to white
 	if (currentImage <= 2) {
 		currentImage = currentImage + 1
 	} else {
 		currentImage = 1
 		$("#imageDiv").css({marginLeft:'600px'})
 	}
-	$("#imageDiv").animate({marginLeft:'-=600px'}, "fast");
-	console.log(currentImage);
-	return(currentImage);
+	$("#imageDiv").animate({marginLeft:'-=600px'}, "fast"); //slide image left
+	$("#dot" + currentImage).css("background-color", "black"); //change new dot colour to black
 };
+
 
