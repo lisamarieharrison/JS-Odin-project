d4274acd7dee6e2d4fe63f70c36405f4bbf55648
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-08-09 16:59:06 +1000

    Puppy sliding now working correctly. Puppies always appear from right.

--- image_slider.js
+++ image_slider.js
@@ -2,18 +2,19 @@ var currentImage = 1;
 
 $(document).ready(function() {
 
-	$('#imageDiv').on('click', next)
+	$('#pictureFrame').on('click', next)
 
 });
 
 function next() {
 	if (currentImage <= 2) {
 		currentImage = currentImage + 1
-		$("#imageDiv").animate({marginLeft:'-=600px'}, "fast");
 	} else {
 		currentImage = 1
-		$("#imageDiv").animate({marginLeft:'+=1200px'}, "fast");
+		$("#imageDiv").css({marginLeft:'600px'})
 	}
+	$("#imageDiv").animate({marginLeft:'-=600px'}, "fast");
 	console.log(currentImage);
 	return(currentImage);
 };
+
